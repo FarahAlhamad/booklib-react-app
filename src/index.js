001b9d76e93167486847a358c429a3498d8a600d
Farah Alhamad <farah.alhamad9@gmail.com>
2023-09-03 21:32:37 +0300

    app initialization

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppProvider } from "./context.";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import BookList from "./components/BookList/BookList";
@@ -9,13 +10,15 @@ import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="about" element={<About />} />
-        <Route path="book" element={<BookList />} />
-        <Route path="/book/:id" element={<BookDetails />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<BookList />} />
+          <Route path="/book/:id" element={<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
